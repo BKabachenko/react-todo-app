666bd91f06aea251bfc4f7e81937c667afccd7a1
Bogdan_ProBook <bogdankabachenko2015@gmail.com>
2025-11-17 14:13:16 +0200

    feat: add todo loading from api, add conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Todo } from "./types";
 import "./App.css";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import AddTodoForm from "./components/AddTodoForm";
 
-const initialTodos: Todo[] = [
+const offlineInitialTodos: Todo[] = [
   { id: 1, text: "Вивчити React", completed: false },
   { id: 2, text: "Вивчити Props", completed: true },
   { id: 3, text: "Вивчити useState", completed: true },
 ];
 
 function App() {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState<Todo[]>(offlineInitialTodos);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10");
+        if (!response.ok) {
+          throw new Error("No response from server");
+        }
+        const data = await response.json();
+        const fetchedTodos: Todo[] = data.map(
+          (item: { id: number; title: string; completed: boolean }) => ({
+            id: item.id,
+            text: item.title,
+            completed: item.completed,
+          })
+        );
+        setTodos(fetchedTodos);
+      } catch (error) {
+        setIsError(true);
+        console.error("Something went wrong!", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
 
   const handleAddTodo = (text: Todo["text"]) => {
     const newTodo: Todo = {
@@ -37,7 +65,12 @@ function App() {
   const handleDeleteTodo = (todoId: Todo["id"]) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id != todoId));
   };
-
+  if (isLoading) {
+    return <Header title="App is loading... please wait." />;
+  }
+  if (isError) {
+    return <Header title="Something went wrong. :(" />;
+  }
   return (
     <>
       <Header title="Мій перший ToDo-список" />
