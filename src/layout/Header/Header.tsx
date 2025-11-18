e9c58e9ec4a21a0739718364b8083fb3a5d75079
Bogdan_ProBook <bogdankabachenko2015@gmail.com>
2025-11-18 14:34:54 +0200

    style: apply CSS module to Header component for scoped styling

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,10 +1,11 @@
+import styles from "./Header.module.scss"
 interface HeaderProps {
   title: string;
 }
 
 const Header = ({title}: HeaderProps) => {
   return (
-    <header className='header'>
+    <header className={styles.header}>
       <h1>{title}</h1>
     </header>
   )
