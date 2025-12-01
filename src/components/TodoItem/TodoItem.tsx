f16b4fdaf569de99b12913ebe4e5c1d4dadd8287
Bogdan_ProBook <bogdankabachenko2015@gmail.com>
2025-12-01 11:14:34 +0200

    style: update TodoItem styles for improved button visibility and delete design

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s from "./TodoItem.module.scss";
 import type { Todo } from "../../types";
-
+import TrashIcon from "../Icons/TrashIcon";
 interface TodoItemProps {
   todo: Todo;
   onToggleTodo: (todoId: Todo["id"]) => void;
@@ -9,29 +9,26 @@ interface TodoItemProps {
 }
 
 const textStyle = (completed: boolean) => {
-  return completed ? s.completed : ""
+  return completed ? s.completed : "";
 };
 
 const TodoItem = ({ todo, onToggleTodo, onDeleteTodo }: TodoItemProps) => {
   return (
     <li className={s.li}>
       <label className={s.checkboxContainer}>
-      <input
-        type="checkbox"
-        className={s.checkbox}
-        checked={todo.completed}
-        onChange={() => onToggleTodo(todo.id)}
-      ></input>
-      <span className={s.checkmark}></span>
+        <input
+          type="checkbox"
+          className={s.checkbox}
+          checked={todo.completed}
+          onChange={() => onToggleTodo(todo.id)}
+        ></input>
+        <span className={s.checkmark}></span>
       </label>
-      <p
-        className={textStyle(todo.completed)}
-        onClick={() => onToggleTodo(todo.id)}
-      >
+      <p className={textStyle(todo.completed)} onClick={() => onToggleTodo(todo.id)}>
         {todo.text}
       </p>
       <button className={s.button} onClick={() => onDeleteTodo(todo.id)}>
-        Delete
+        <TrashIcon className={s.trashIcon} />
       </button>
     </li>
   );
