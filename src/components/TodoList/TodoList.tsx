f248703a65937866afd3f1c66e15e018cfc96075
Bogdan_ProBook <bogdankabachenko2015@gmail.com>
2025-12-02 14:28:38 +0200

    style: implement cool deleting animation

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,8 @@
 import type { Todo } from "../../types";
 import TodoItem from "../TodoItem/TodoItem";
 import s from "./TodoList.module.scss"
+import { useAutoAnimate } from '@formkit/auto-animate/react'
+
 interface TodoListProps {
   todos: Todo[];
   onToggleTodo: (todoId: Todo["id"]) => void;
@@ -8,8 +10,10 @@ interface TodoListProps {
 }
 
 const TodoList = ({ todos, onToggleTodo, onDeleteTodo }: TodoListProps) => {
+  const [parent] = useAutoAnimate();
+
   return (
-    <ul className={s.ul}>
+    <ul className={s.ul} ref={parent}>
       {todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} onToggleTodo={onToggleTodo} onDeleteTodo={onDeleteTodo} />
       ))}
